fa5377961907d713af90d9d1d6536b9a95214efa
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2020-09-23 21:18:29 +0530

    Spring boot hello world app

--- Spring Boot/demo/src/main/java/com/example/demo/controllers/HelloController.java
+++ Spring Boot/demo/src/main/java/com/example/demo/controllers/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -10,6 +11,8 @@ public class HelloController {
     @GetMapping("/hello") //http://localhost:8080/hello?name=Dolly
     public String sayHello(@RequestParam(required = false,
             defaultValue = "World") String name, Model model) {
+
+        model.addAttribute("user",name);
         return "hello";
     }
 
