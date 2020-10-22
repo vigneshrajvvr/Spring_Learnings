def019934ca860d01b46feb0c5add7102ff9edf1
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2020-10-22 21:34:14 +0530

    Spring security introduction using spring boot

---
+++ Spring Security/Introduction/src/main/java/com/practice/vvr/security/introduction/Introduction/IntroductionApplication.java
@@ -0,0 +1,13 @@
+package com.practice.vvr.security.introduction.Introduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionApplication.class, args);
+	}
+
+}
---
+++ Spring Security/Introduction/src/test/java/com/practice/vvr/security/introduction/Introduction/IntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.vvr.security.introduction.Introduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
