8d25221da0923b437ac9c49a76baf6b118b7ddac
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2020-10-22 20:43:14 +0530

    Update and delete employees

---
+++ Spring Boot/36-thymeleafdemo-employees-update/src/main/java/com/practice/vvr/springboot/thymeleafdemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,60 @@
+package com.practice.vvr.springboot.thymeleafdemo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.practice.vvr.springboot.thymeleafdemo.dao.EmployeeRepository;
+import com.practice.vvr.springboot.thymeleafdemo.entity.Employee;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+	
+	private EmployeeRepository employeeRepository;
+	
+	@Autowired
+	public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+		this.employeeRepository = employeeRepository;
+	}
+
+	// No need to @Transactional annotation
+	// Spring Data API provides it.
+	
+	@Override
+	public List<Employee> findAll() {
+		return employeeRepository.findAllByOrderByLastName();
+	}
+
+	@Override
+	public Employee findById(int theId) {
+		Optional<Employee> result = employeeRepository.findById(theId);
+		
+		Employee theEmployee = null;
+		
+		if(result.isPresent()) {
+			theEmployee = result.get();
+		} else {
+			// didn't find the employee
+			throw new RuntimeException("Did not find employee id - " + theId);
+		}
+		
+		return theEmployee;
+	}
+
+	@Override
+	public void save(Employee theEmployee) {
+		
+		employeeRepository.save(theEmployee);
+		
+	}
+
+	@Override
+	public void deleteById(int theId) {	
+		
+		employeeRepository.deleteById(theId);
+				
+	}
+
+}
