a2aad54010bbfac7b663fd1cc5ce24c24d9e4d2a
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2020-11-02 00:02:34 +0530

    Dependency Injection

--- Spring Security/Introduction/src/main/java/com/practice/vvr/security/introduction/Introduction/IntroductionApplication.java
+++ Spring Security/Introduction/src/main/java/com/practice/vvr/security/introduction/Introduction/IntroductionApplication.java
@@ -2,18 +2,33 @@ package com.practice.vvr.security.introduction.Introduction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+// Dependency Injection
+ 
 @SpringBootApplication
 public class IntroductionApplication {
+	
+	// Manually specifying creation of objects
+//	@Bean
+//	Foo foo() {
+//		return new Foo();
+//	}
+	
+	@Bean
+	Bar bar(Foo foo) {
+		return new Bar(foo);
+	}
+	
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {	
 		SpringApplication.run(IntroductionApplication.class, args);
 	}
 
 }
 
-@Component
+//@Component
 class Bar {
 	
 	// dependency injection
