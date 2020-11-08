a0d3564ae8be549d74c4c30c84fafc7973a12810
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2020-11-08 20:45:40 +0530

    Spring expression language

--- Spring Security/Introduction/src/main/java/com/practice/vvr/security/introduction/Introduction/IntroductionApplication.java
+++ Spring Security/Introduction/src/main/java/com/practice/vvr/security/introduction/Introduction/IntroductionApplication.java
@@ -1,8 +1,12 @@
 package com.practice.vvr.security.introduction.Introduction;
 
+import java.util.UUID;
+
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 // Dependency Injection
@@ -11,15 +15,18 @@ import org.springframework.stereotype.Component;
 public class IntroductionApplication {
 	
 	// Manually specifying creation of objects
-//	@Bean
-//	Foo foo() {
-//		return new Foo();
-//	}
+	//	@Bean
+	//	Foo foo() {
+	//		return new Foo();
+	//	}
 	
-	@Bean
-	Bar bar(Foo foo) {
-		return new Bar(foo);
-	}
+	// To create an object with uuid associated with it
+	// Object of the UuidService class is being accessed by the component name "uuid"
+	// @Value annotation binds the value with the parameter uuid
+	//	@Bean
+	//	Bar bar(Foo foo, @Value("#{uuid.buildUuid()}") String uuid) {
+	//		return new Bar(foo, uuid);
+	//	}
 	
 
 	public static void main(String[] args) {	
@@ -28,14 +35,30 @@ public class IntroductionApplication {
 
 }
 
-//@Component
+//To provide uuid 
+	@Component("uuid")
+	class UuidService {
+		
+		public String buildUuid() {
+			return UUID.randomUUID().toString();
+		}
+		
+	}
+
+@Component
 class Bar {
 	
 	// dependency injection
 	private final Foo foo;
 	
-	public Bar(Foo foo) {
+	private final Log log = LogFactory.getLog(getClass());
+	
+	// Spring expression language - to evaluate expressions at runtimes
+	public Bar(Foo foo, @Value("#{uuid.buildUuid()}") String uuid,
+			@Value("#{ 2 > 1}") boolean proceed) {
 		this.foo = foo;
+		this.log.info("UUID " + uuid);
+		this.log.info("Proceed " + proceed);
 	}
 	
 }
