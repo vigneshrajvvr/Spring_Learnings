650b94469ed666f9664ec38177c4c1665f3a82c4
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2020-11-11 23:13:19 +0530

    Spring AOP and Servlet filters

--- Spring Security/Introduction/src/main/java/com/practice/vvr/security/introduction/IntroductionApplication.java
+++ Spring Security/Introduction/src/main/java/com/practice/vvr/security/introduction/IntroductionApplication.java
@@ -1,9 +1,19 @@
-package com.practice.vvr.security.introduction.Introduction;
+package com.practice.vvr.security.introduction;
 
+import java.io.IOException;
 import java.util.UUID;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +21,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,6 +58,57 @@ public class IntroductionApplication {
 
 }
 
+// Servlet filter
+@Component
+class LoggingFilter implements javax.servlet.Filter {
+	
+	private final Log log = LogFactory.getLog(getClass());
+
+	// To intercept incoming http request
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		Assert.isTrue(request instanceof HttpServletRequest,"This assumes you have an HTTP request");
+		
+		HttpServletRequest httpServletRequest = HttpServletRequest.class.cast(request);
+		
+		String uri = httpServletRequest.getRequestURI();
+		
+		this.log.info("New request for "+ uri);
+		
+		long time = System.currentTimeMillis();
+		
+		chain.doFilter(request, response);
+		
+		long delta = System.currentTimeMillis() - time;
+		
+		this.log.info("Request for " + uri + " took " + delta + "ms");
+		
+	}
+}
+
+// Spring AOP - @Around 
+@Component
+@Aspect
+class LoggingAspect {
+	
+	private final Log log = LogFactory.getLog(getClass());
+	
+	@Around("execution( * com.practice.vvr..*.*(..) )")
+	public Object log(ProceedingJoinPoint pjp) throws Throwable {
+		// Before
+		this.log.info("Before " + pjp.toString());
+		Object object = pjp.proceed();
+		// After
+		this.log.info("After " + pjp.toString());
+		return object;
+	}
+	
+}
+
+
+// Rest controller to access google books api and get book details based on isbn 
 @RestController
 class IsbnRestController {
 	
