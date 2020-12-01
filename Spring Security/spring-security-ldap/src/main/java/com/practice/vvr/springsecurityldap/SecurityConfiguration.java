63ef100058ed97784e8b83fcbfda52d81e769ff4
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2020-12-01 23:12:23 +0530

    LDAP authentication - Lightweight Directory Access Protocol

---
+++ Spring Security/spring-security-ldap/src/main/java/com/practice/vvr/springsecurityldap/SecurityConfiguration.java
@@ -0,0 +1,41 @@
+package com.practice.vvr.springsecurityldap;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+		@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+			auth.ldapAuthentication()
+				.userDnPatterns("uid={0},ou=people")    // format of dn in ldif file
+				.groupSearchBase("ou=groups")
+				.contextSource()
+				.url("ldap://localhost:8399/dc=springframework,dc=org")
+		        .and()
+		        .passwordCompare()
+		        .passwordEncoder(new BCryptPasswordEncoder())
+		        .passwordAttribute("userPassword"); 
+			
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http
+			.authorizeRequests()
+			.anyRequest().fullyAuthenticated()
+			.and()
+			.formLogin();
+		
+	}
+	
+	
+	
+}
