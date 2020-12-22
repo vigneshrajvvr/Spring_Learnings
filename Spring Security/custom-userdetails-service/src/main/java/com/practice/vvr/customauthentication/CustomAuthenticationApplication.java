9dc80d92288f0f5373a2f08c9e8dd575a3af895a
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2020-12-22 23:52:41 +0530

    Custom user details service

--- Spring Security/custom-userdetails-service/src/main/java/com/practice/vvr/customauthentication/CustomAuthenticationApplication.java
+++ Spring Security/custom-userdetails-service/src/main/java/com/practice/vvr/customauthentication/CustomAuthenticationApplication.java
@@ -1,6 +1,11 @@
 package com.practice.vvr.customauthentication;
 
 import java.security.Principal;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -51,10 +57,70 @@ class CustomSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 @Service
 class CustomUserDetailsService implements UserDetailsService {
-
+	
+	private final Map<String, UserDetails> users= new ConcurrentHashMap<>();
+	
+	public CustomUserDetailsService(Collection<UserDetails> seedUsers) {
+		seedUsers.forEach(user -> this.users.put(user.getUsername(), user));
+	}
+	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		if(this.users.containsKey(username)) {
+			return this.users.get(username);
+		}
+		
+		throw new UsernameNotFoundException("Couldn't find " + username);
+	}
+	
+}
+
+class CustomUserDetails implements UserDetails {
+	
+	private final Set<GrantedAuthority> authorities = new HashSet<>();
+	private final String username, password;
+	private final boolean active;
+	
+	public CustomUserDetails(String username, String password, boolean active, String ... authorities) {
+		this.username = username;
+		this.password = password;
+		this.active = active;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
 		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return false;
 	}
 	
 }
