66637d857fe0f0ea4396b2518e06ae71227eae41
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-02-09 23:51:20 +0530

    Download files from a folder

--- Spring Boot Fundamentals Course Udemy/filehandling/src/main/java/com/example/springboot/filehandling/controller/FileUploadController.java
+++ Spring Boot Fundamentals Course Udemy/filehandling/src/main/java/com/example/springboot/filehandling/controller/FileUploadController.java
@@ -1,15 +1,22 @@
 package com.example.springboot.filehandling.controller;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.logging.Logger;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
@@ -41,7 +48,46 @@ public class FileUploadController {
 		
 		model.addAttribute("msg", "Successfully uploaded the files " + fileNames.toString());
 		
-		return "uploadView";		
+		return "uploadView"; 		
+	}
+	
+	@RequestMapping("/downloadView")
+	public String showFiles(Model model) {
+		File folder = new File(uploadDirectory);
+		File[] listOfFiles = folder.listFiles();
+		model.addAttribute("files", listOfFiles);
+		return "showfiles";
+	}
+	
+	@RequestMapping("/file/{fileName}")
+	@ResponseBody
+	public void show(@PathVariable("fileName") String fileName, HttpServletResponse response) {
+		if(fileName.indexOf(".doc") > -1) {
+			response.setContentType("application/msword");
+		}
+		if(fileName.indexOf(".docx") > -1) response.setContentType("application/msword");
+		if(fileName.indexOf(".xls") > -1) response.setContentType("application/vnd.ms-excel");
+		if(fileName.indexOf(".csv") > -1) response.setContentType("application/vnd.ms-excel");
+		if(fileName.indexOf(".ppt") > -1) response.setContentType("application/ppt");
+		if(fileName.indexOf(".pdf") > -1) response.setContentType("application/pdf");
+		if(fileName.indexOf(".zip") > -1) response.setContentType("application/zip");
+		if(fileName.indexOf(".jpg") > -1) response.setContentType("image/jpeg");
+		if(fileName.indexOf(".PNG") > -1) response.setContentType("image/png");
+		response.setHeader("Content-Disposition", "attachment: filename="+fileName);
+		response.setHeader("Content-Tranfer-Encoding", "Binary");
+		try {
+			BufferedOutputStream bos = new BufferedOutputStream(response.getOutputStream());
+			FileInputStream fis = new FileInputStream(uploadDirectory + "\\" + fileName);
+			int len;
+			byte[] buf = new byte[1024];
+			while((len = fis.read(buf)) > 0) {
+				bos.write(buf, 0, len);
+			}
+			bos.close();
+			response.flushBuffer();
+		} catch(IOException ex) {
+			ex.printStackTrace();
+		}
 	}
 
 }
--- Spring Boot Fundamentals Course Udemy/productrestapi/src/main/java/com/practice/vvr/springboot/controller/ProductController.java
+++ Spring Boot Fundamentals Course Udemy/productrestapi/src/main/java/com/practice/vvr/springboot/controller/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.practice.vvr.springboot.entity.Product;
@@ -16,6 +17,10 @@ import com.practice.vvr.springboot.repository.ProductRepository;
 public class ProductController {
 	
 	private ProductRepository productRepository;
+	
+	public ProductController(ProductRepository productRepository) {
+		this.productRepository = productRepository;
+	}
 	
 	@GetMapping("/products")
 	public List<Product> getProducts() {
@@ -28,7 +33,7 @@ public class ProductController {
 	}
 	
 	@PostMapping("/products")
-	public Product createProduct(Product product) {
+	public Product createProduct(@RequestBody Product product) {
 		return productRepository.save(product);
 	}
 	
