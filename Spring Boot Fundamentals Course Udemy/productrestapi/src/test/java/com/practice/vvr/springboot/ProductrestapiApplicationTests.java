986b26bb4799cbdf634e1117b3e167116634ee7f
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-02-15 22:41:26 +0530

    Profiles in spring boot

--- Spring Boot Fundamentals Course Udemy/productrestapi/src/test/java/com/practice/vvr/springboot/ProductrestapiApplicationTests.java
+++ Spring Boot Fundamentals Course Udemy/productrestapi/src/test/java/com/practice/vvr/springboot/ProductrestapiApplicationTests.java
@@ -1,7 +1,10 @@
 package com.practice.vvr.springboot;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,11 +12,15 @@ import com.practice.vvr.springboot.entity.Product;
 
 @SpringBootTest
 class ProductrestapiApplicationTests {
+	
+	@Value("${productrestapi.services.url}")
+	private String baseURL;
 
 	@Test
 	void testGetProduct() {
+		System.out.println(baseURL);
 		RestTemplate restTemplate = new RestTemplate();
-		Product product = restTemplate.getForObject("http://localhost:8081/productsapi/products/1", Product.class);
+		Product product = restTemplate.getForObject(baseURL + "1", Product.class);
 		assertNotNull(product);
 		assertEquals("IPhone", product.getName());
 	}
@@ -25,7 +32,7 @@ class ProductrestapiApplicationTests {
 		product.setName("Samsung Mobile");
 		product.setDescription("It's Awesome");
 		product.setPrice(1000);
-		Product newProduct = restTemplate.postForObject("http://localhost:8081/productsapi/products", product, Product.class);
+		Product newProduct = restTemplate.postForObject(baseURL, product, Product.class);
 		assertNotNull(newProduct);
 		assertNotNull(newProduct.getId());
 		assertEquals("Samsung Mobile", newProduct.getName());
@@ -34,7 +41,7 @@ class ProductrestapiApplicationTests {
 	@Test
 	void testUpadateProduct() {
 		RestTemplate restTemplate = new RestTemplate();
-		Product product = restTemplate.getForObject("http://localhost:8081/productsapi/products/1", Product.class);
+		Product product = restTemplate.getForObject(baseURL + "1", Product.class);
 		product.setPrice(2000);
 		restTemplate.put("http://localhost:8081/productsapi/products/", product);
 		
