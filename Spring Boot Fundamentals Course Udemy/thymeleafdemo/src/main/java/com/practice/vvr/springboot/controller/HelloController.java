bccbae5adaa1fc380ab43a298365ff2b9107b332
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-02-24 23:51:51 +0530

    Thymeleaf demo

--- Spring Boot Fundamentals Course Udemy/thymeleafdemo/src/main/java/com/practice/vvr/springboot/controller/HelloController.java
+++ Spring Boot Fundamentals Course Udemy/thymeleafdemo/src/main/java/com/practice/vvr/springboot/controller/HelloController.java
@@ -1,8 +1,14 @@
 package com.practice.vvr.springboot.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.practice.vvr.springboot.model.Student;
 
 @Controller
 public class HelloController {
@@ -16,6 +22,49 @@ public class HelloController {
 	public ModelAndView sendData() {
 		ModelAndView mav = new ModelAndView("data");
 		mav.addObject("message", "Take up one idea and make it your life");
+		return mav;
+	}
+	
+	@RequestMapping("/student")
+	public ModelAndView getStudent() {
+		ModelAndView mav = new ModelAndView("student");
+		Student s = new Student();
+		s.setName("John");
+		s.setScore(98);
+		mav.addObject("student", s);
+		return mav;
+	}
+	
+	@RequestMapping("/students")
+	public ModelAndView getStudents() {
+		ModelAndView mav = new ModelAndView("studentList");
+		Student s1 = new Student();
+		s1.setName("John");
+		s1.setScore(98);
+		
+		Student s2 = new Student();
+		s2.setName("Bob");
+		s2.setScore(90);
+		
+		List<Student> students = Arrays.asList(s1, s2);	
+		
+		mav.addObject("students", students);
+		
+		return mav;
+	}
+	
+	@RequestMapping("/studentForm")
+	public ModelAndView displayStudentForm() {
+		ModelAndView mav = new ModelAndView("studentForm");
+		mav.addObject("student", new Student());
+		return mav;
+	}
+	
+	@RequestMapping("/saveStudent")
+	public ModelAndView saveStudent(@ModelAttribute Student student) {
+		ModelAndView mav = new ModelAndView("result");
+		System.out.println(student.getName());
+		System.out.println(student.getScore());
 		return mav;
 	}
 
