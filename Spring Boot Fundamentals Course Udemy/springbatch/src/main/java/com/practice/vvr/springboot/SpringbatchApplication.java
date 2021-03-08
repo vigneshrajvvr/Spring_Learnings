2e0a7a962d94259717532f53584a0292860c5066
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-03-08 23:42:38 +0530

    Spring batch project

---
+++ Spring Boot Fundamentals Course Udemy/springbatch/src/main/java/com/practice/vvr/springboot/SpringbatchApplication.java
@@ -0,0 +1,17 @@
+package com.practice.vvr.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * Spring batch project
+ */
+
+@SpringBootApplication
+public class SpringbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbatchApplication.class, args);
+	}
+
+}
