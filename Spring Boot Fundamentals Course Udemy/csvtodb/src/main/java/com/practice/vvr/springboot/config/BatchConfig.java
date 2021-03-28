2e0ec9e5bcac9456cb025031209844244c704a0d
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-03-28 23:32:46 +0530

    csv to database using spring batch

--- Spring Boot Fundamentals Course Udemy/csvtodb/src/main/java/com/practice/vvr/springboot/config/BatchConfig.java
+++ Spring Boot Fundamentals Course Udemy/csvtodb/src/main/java/com/practice/vvr/springboot/config/BatchConfig.java
@@ -1,4 +1,11 @@
 package com.practice.vvr.springboot.config;
+import javax.sql.DataSource;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
@@ -8,14 +15,40 @@ import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import com.practice.vvr.springboot.model.Product;
 
 @Configuration
 public class BatchConfig {
+	
+	@Autowired
+	private JobBuilderFactory jbf;
+	
+	@Autowired
+	private StepBuilderFactory stf;
+	
+	@Bean
+	public Job job() {
+		return jbf.get("job1")
+		       .incrementer(new RunIdIncrementer())
+		       .start(step())
+		       .build();
+	}
+	
+	@Bean
+	public Step step() {
+		return stf.get("step1")
+				  .<Product,Product>chunk(3)
+				  .reader(reader())
+				  .processor(processor())
+				  .writer(writer())
+				  .build();
+	}
 	
 	@Bean
 	public ItemReader<Product> reader() {
@@ -49,9 +82,21 @@ public class BatchConfig {
 	@Bean
 	public ItemWriter<Product> writer() {
 		JdbcBatchItemWriter<Product> writer = new JdbcBatchItemWriter<>();
+		writer.setDataSource(dataSource());
 		writer.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>());
 		writer.setSql("INSERT INTO PRODUCT (ID,NAME,DESCRIPTION,PRICE) VALUES (:id,:name,:description,:price)");
 		return writer;
+	}
+	
+	@Bean
+	public DataSource dataSource() {
+		
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("https://localhost:3306/mydb");
+		dataSource.setUsername("springstudent");
+		dataSource.setPassword("springstudent");
+		return dataSource;
 	}
 
 }
