80470d4ecbf9265f8b055d2263ad305dc4d59470
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-03-31 23:02:02 +0530

    Modifications

--- Spring Boot Fundamentals Course Udemy/productrestapi/src/main/java/com/practice/vvr/springboot/controller/ProductController.java
+++ Spring Boot Fundamentals Course Udemy/productrestapi/src/main/java/com/practice/vvr/springboot/controller/ProductController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.transaction.annotation.Transactional;
