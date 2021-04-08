b57c05567113df308b52d30fa52bba924ca6f6bd
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-04-08 23:00:32 +0530

    Unit testing find all products url

--- Spring Boot Fundamentals Course Udemy/productrestapi/src/test/java/com/practice/vvr/springboot/ProductrestapiMockMvcTests.java
+++ Spring Boot Fundamentals Course Udemy/productrestapi/src/test/java/com/practice/vvr/springboot/ProductrestapiMockMvcTests.java
@@ -1,22 +1,27 @@
 package com.practice.vvr.springboot;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.practice.vvr.springboot.entity.Product;
 import com.practice.vvr.springboot.repository.ProductRepository;
 
-@SpringBootTest
-public class ProductRestControllerMvcTest {
-	
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class ProductrestapiMockMvcTests {
+
 	/*
 	 * Unit testing
 	 */
@@ -28,7 +33,7 @@ public class ProductRestControllerMvcTest {
 	private ProductRepository productRepository;
 	
 	@Test
-	public void testFindAll() {
+	public void testFindAll() throws Exception{
 		Product product = new Product();
 		product.setId(1);
 		product.setName("Macbook");
@@ -36,6 +41,10 @@ public class ProductRestControllerMvcTest {
 		List<Product> products = new ArrayList<>();
 		products.add(product);
 	    when(productRepository.findAll()).thenReturn(products);
+	    
+	    mockMvc.perform(get("/productsapi/products").contextPath("/productsapi")).andExpect(status().isOk());
 	}
-
 }
+
+
+
