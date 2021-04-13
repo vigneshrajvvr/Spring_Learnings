f438dc2845a5dc6d00cf4f03ada9926469b1676f
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-04-13 21:31:06 +0530

    Unit testing - All products

--- Spring Boot Fundamentals Course Udemy/productrestapi/src/test/java/com/practice/vvr/springboot/ProductrestapiMockMvcTests.java
+++ Spring Boot Fundamentals Course Udemy/productrestapi/src/test/java/com/practice/vvr/springboot/ProductrestapiMockMvcTests.java
@@ -2,6 +2,7 @@ package com.practice.vvr.springboot;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.ArrayList;
@@ -15,6 +16,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.practice.vvr.springboot.entity.Product;
 import com.practice.vvr.springboot.repository.ProductRepository;
 
@@ -25,26 +28,44 @@ public class ProductrestapiMockMvcTests {
 	/*
 	 * Unit testing
 	 */
-	
+
+	private static final String PRODUCTS_URL = "/productsapi/products";
+
+	private static final String CONTEXT_URL = "/productsapi";
+
+	private static final int PRODUCT_PRICE = 1000;
+
+	private static final String PRODUCT_DESCRIPTION = "Its awesome";
+
+	private static final String PRODUCT_NAME = "Macbook";
+
+	private static final int PRODUCT_ID = 1;
+
 	@Autowired
 	private MockMvc mockMvc;
-	
+
 	@MockBean
 	private ProductRepository productRepository;
-	
+
 	@Test
-	public void testFindAll() throws Exception{
-		Product product = new Product();
-		product.setId(1);
-		product.setName("Macbook");
-		product.setDescription("It's awesome");
+	public void testFindAll() throws Exception {
+		Product product = buildProduct();
 		List<Product> products = new ArrayList<>();
 		products.add(product);
-	    when(productRepository.findAll()).thenReturn(products);
-	    
-	    mockMvc.perform(get("/productsapi/products").contextPath("/productsapi")).andExpect(status().isOk());
+		when(productRepository.findAll()).thenReturn(products);
+		
+		ObjectWriter objectWriter = new ObjectMapper().writer().withDefaultPrettyPrinter();
+
+		mockMvc.perform(get(PRODUCTS_URL).contextPath(CONTEXT_URL)).andExpect(status().isOk())
+				.andExpect(content().json(objectWriter.writeValueAsString(products)));
+	}
+
+	private Product buildProduct() {
+		Product product = new Product();
+		product.setId(PRODUCT_ID);
+		product.setName(PRODUCT_NAME);
+		product.setDescription(PRODUCT_DESCRIPTION);
+		product.setPrice(PRODUCT_PRICE);
+		return product;
 	}
 }
-
-
-
