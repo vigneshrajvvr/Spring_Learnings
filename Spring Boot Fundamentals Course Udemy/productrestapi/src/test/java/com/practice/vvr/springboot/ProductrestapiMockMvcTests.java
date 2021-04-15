6333bdb2351c8611abbcbc7c3cac82e8df8b65bf
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-04-15 23:47:22 +0530

    Unit testing of CRUD operations

--- Spring Boot Fundamentals Course Udemy/productrestapi/src/test/java/com/practice/vvr/springboot/ProductrestapiMockMvcTests.java
+++ Spring Boot Fundamentals Course Udemy/productrestapi/src/test/java/com/practice/vvr/springboot/ProductrestapiMockMvcTests.java
@@ -1,7 +1,12 @@
 package com.practice.vvr.springboot;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -13,9 +18,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.practice.vvr.springboot.entity.Product;
@@ -53,11 +60,44 @@ public class ProductrestapiMockMvcTests {
 		List<Product> products = new ArrayList<>();
 		products.add(product);
 		when(productRepository.findAll()).thenReturn(products);
-		
+
 		ObjectWriter objectWriter = new ObjectMapper().writer().withDefaultPrettyPrinter();
 
 		mockMvc.perform(get(PRODUCTS_URL).contextPath(CONTEXT_URL)).andExpect(status().isOk())
 				.andExpect(content().json(objectWriter.writeValueAsString(products)));
+	}
+
+	@Test
+	public void testGetProductById() {
+		Product product = buildProduct();
+		when(productRepository.findById(PRODUCTS_URL)).thenReturn(product);
+	}
+
+	@Test
+	public void testCreateProduct() throws JsonProcessingException, Exception {
+		Product product = buildProduct();
+		when(productRepository.save(any())).thenReturn(product);
+
+		ObjectWriter objectWriter = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		mockMvc.perform(post(PRODUCTS_URL).contextPath(CONTEXT_URL).contentType(MediaType.APPLICATION_JSON)
+				.content(objectWriter.writeValueAsString(product))).andExpect(status().isOk());
+	}
+
+	@Test
+	public void testUpdateProduct() throws JsonProcessingException, Exception {
+		Product product = buildProduct();
+		product.setPrice(1200);
+		when(productRepository.save(any())).thenReturn(product);
+
+		ObjectWriter objectWriter = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		mockMvc.perform(put(PRODUCTS_URL).contextPath(CONTEXT_URL).contentType(MediaType.APPLICATION_JSON)
+				.content(objectWriter.writeValueAsString(product))).andExpect(status().isOk());
+	}
+
+	@Test
+	public void testDeleteProduct() throws Exception {
+		doNothing().when(productRepository).deleteById(PRODUCT_ID);
+		mockMvc.perform(delete(PRODUCTS_URL + PRODUCT_ID).contextPath(CONTEXT_URL)).andExpect(status().isOk());
 	}
 
 	private Product buildProduct() {
