1cd6bb6866621d586bcc77bf107e5a17ec9bec88
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-04-25 21:32:52 +0530

    Bank application setup

---
+++ Spring Security/EasyBankApplication/src/main/java/com/practice/vvr/security/EasyBankApplication.java
@@ -0,0 +1,13 @@
+package com.practice.vvr.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyBankApplication.class, args);
+	}
+
+}
---
+++ Spring Security/EasyBankApplication/src/test/java/com/practice/vvr/security/EasyBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.vvr.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
