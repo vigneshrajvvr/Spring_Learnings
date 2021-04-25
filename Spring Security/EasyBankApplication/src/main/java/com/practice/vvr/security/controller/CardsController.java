31f2079dbe985dba3c90c159e347fa9616a393c2
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-04-25 21:32:19 +0530

    Bank application setup

---
+++ Spring Security/EasyBankApplication/src/main/java/com/practice/vvr/security/controller/CardsController.java
@@ -0,0 +1,12 @@
+package com.practice.vvr.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CardsController {
+	@GetMapping("/myCards")
+	public String getCardDetails(String input) {
+		return "Here are the card details from the DB";
+	}
+}
