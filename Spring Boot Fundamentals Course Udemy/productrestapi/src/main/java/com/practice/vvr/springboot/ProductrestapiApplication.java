3e8024599d97d9cb2e0aa71fa8f19c0fb28d45be
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-05-12 23:18:02 +0530

    Swagger introduction

--- Spring Boot Fundamentals Course Udemy/productrestapi/src/main/java/com/practice/vvr/springboot/ProductrestapiApplication.java
+++ Spring Boot Fundamentals Course Udemy/productrestapi/src/main/java/com/practice/vvr/springboot/ProductrestapiApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 //@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class}) 
 //To disable auto configuration for data source
 @EnableCaching
+@EnableSwagger2            //To enable usage of swagger
 public class ProductrestapiApplication {
 
 	public static void main(String[] args) {
