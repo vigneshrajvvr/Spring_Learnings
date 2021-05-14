186b07e824018e8f31a490977119f19e65889491
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-05-14 23:41:50 +0530

    File upload and download using Spring REST

---
+++ Spring Boot Fundamentals Course Udemy/RestFileProcessing/src/main/java/com/practice/vvr/springboot/RestFileProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.practice.vvr.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFileProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFileProcessingApplication.class, args);
+	}
+
+}
