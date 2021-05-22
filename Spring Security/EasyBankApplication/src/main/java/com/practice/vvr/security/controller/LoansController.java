391e9f393144136b723ad72231137f587159e709
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-05-22 23:33:51 +0530

    Basic security and controller setup

---
+++ Spring Security/EasyBankApplication/src/main/java/com/practice/vvr/security/controller/LoansController.java
@@ -0,0 +1,14 @@
+package com.practice.vvr.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoansController {
+	
+	@GetMapping("/myLoans")
+	public String getLoanDetails(String input) {
+		return "Here are the loan details from the DB";
+	}
+	
+}
