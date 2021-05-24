7bb49920d1f383617e91aca5c3f27229f2da10d4
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-05-24 22:57:08 +0530

    Path Variable and User class creation

--- Microservices & Spring Rest Recap/Spring_Rest/src/main/java/com/practice/vvr/controller/HelloWorldController.java
+++ Microservices & Spring Rest Recap/Spring_Rest/src/main/java/com/practice/vvr/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.practice.vvr.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.practice.vvr.model.HelloWorldBean;
@@ -19,6 +20,11 @@ public class HelloWorldController {
 	@GetMapping("/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World!!");
+	}
+
+	@GetMapping("/hello-world-bean/{beanName}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable("beanName") String beanName) {
+		return new HelloWorldBean("Hello World!! " + beanName);
 	}
 	
 }
