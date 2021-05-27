6d7d313ae0385ecbe509f69a462f5306303085ce
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-05-27 23:07:19 +0530

    Rest controller to retrieve all and specific user

---
+++ Microservices & Spring Rest Recap/Spring_Rest/src/main/java/com/practice/vvr/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.practice.vvr.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.practice.vvr.model.User;
+import com.practice.vvr.repository.UserDaoService;
+
+@RestController
+public class UserController {
+	
+	@Autowired
+	private UserDaoService userDaoService;
+	
+	//GET /users
+	//retrieveAllUsers
+	@GetMapping("/users")
+	public List<User> retrieveAllUsers() {
+		return userDaoService.findAll();
+	}
+	
+	//GET /users/{id}
+	//retrieveUser(int id)
+	@GetMapping("/users/{id}")
+	public User retrieveUser(@PathVariable("id") int id) {
+		return userDaoService.findOne(id);
+	}
+	
+	
+	
+}
