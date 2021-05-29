fe8e352e7913a191040cf404497a4ab1f9e61dfb
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-05-30 01:01:20 +0530

    Permit and deny all the requests

--- Spring Security/EasyBankApplication/src/main/java/com/practice/vvr/security/config/ProjectSecurityConfig.java
+++ Spring Security/EasyBankApplication/src/main/java/com/practice/vvr/security/config/ProjectSecurityConfig.java
@@ -8,26 +8,53 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	/*
-	 * /myAccount - Secured 
-	 * /myBalance - Secured 
-	 * /myLoans   - Secured 
-	 * /myCards   - Secured 
-	 * /notices - Not secured 
-	 * /contact - Not secured
+	 * /myAccount - Secured /myBalance - Secured /myLoans - Secured /myCards -
+	 * Secured /notices - Not secured /contact - Not secured
 	 * 
 	 */
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http
-		    .authorizeRequests()
-				.antMatchers("/myAccount").authenticated()
-				.antMatchers("/myBalance").authenticated()
-				.antMatchers("/myLoans").authenticated()
-				.antMatchers("/myCards").authenticated()
-				.antMatchers("/notices").permitAll()
-				.antMatchers("/contact").permitAll();
-		http.formLogin();
-		http.httpBasic();
+
+		/*
+		 * Default configuration
+		 * 
+		 * http .authorizeRequests() .anyRequest().authenticated(); http.formLogin();
+		 * http.formBasic();
+		 */
+
+		/*
+		 * Custom configuration as per our requirement
+		 * 
+		 * 
+		 * http .authorizeRequests() .antMatchers("/myAccount").authenticated()
+		 * .antMatchers("/myBalance").authenticated()
+		 * .antMatchers("/myLoans").authenticated()
+		 * .antMatchers("/myCards").authenticated() .antMatchers("/notices").permitAll()
+		 * .antMatchers("/contact").permitAll(); http.formLogin(); http.httpBasic();
+		 */
+		
+		/*
+		 * formLogin() - Request from browser login form httpBasic() - Request from
+		 * other API
+		 */
+
+		/*
+		 * Configuration to deny all the requests
+		 *
+		 * http
+		 * 	 .authorizeRequests().anyRequest().denyAll();
+		 * http.formLogin();
+		 * http.httpBasic();
+		 */
+		
+		/*
+		 * Configuration to permit all the requests
+		 */
+		  http
+		  	 .authorizeRequests().anyRequest().permitAll();
+		  http.formLogin();
+		  http.httpBasic();
+		 
 	}
 
 }
