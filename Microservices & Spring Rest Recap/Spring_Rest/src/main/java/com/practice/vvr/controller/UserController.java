ced6ab25db6e8993e93f5f4a4680e4d207c48016
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-06-03 23:05:46 +0530

    Return response object in POST request and specific exception handling

--- Microservices & Spring Rest Recap/Spring_Rest/src/main/java/com/practice/vvr/controller/UserController.java
+++ Microservices & Spring Rest Recap/Spring_Rest/src/main/java/com/practice/vvr/controller/UserController.java
@@ -1,14 +1,18 @@
 package com.practice.vvr.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.practice.vvr.controller.exception.UserNotFoundException;
 import com.practice.vvr.model.User;
 import com.practice.vvr.repository.UserDaoService;
 
@@ -29,14 +33,26 @@ public class UserController {
 	//retrieveUser(int id)
 	@GetMapping("/users/{id}")
 	public User retrieveUser(@PathVariable("id") int id) {
-		return userDaoService.findOne(id);
+		User user = userDaoService.findOne(id);
+		if(user == null) {
+			throw new UserNotFoundException("Id-"+id);
+		}
+		return user;
 	}
 	
 	//input - details of user
 	//output - Created response & return the created uri
 	@PostMapping("/users")
-	public void createUser(@RequestBody User user) {
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
 		User savedUser = userDaoService.save(user);
+		
+		URI location = ServletUriComponentsBuilder
+									.fromCurrentRequest()
+									.path("/{id}")
+									.buildAndExpand(savedUser.getId())
+									.toUri();
+		
+		return ResponseEntity.created(location).build();
 	}
 	
 }
---
+++ Microservices & Spring Rest Recap/Spring_Rest/src/main/java/com/practice/vvr/controller/exception/UserNotFoundException.java
@@ -0,0 +1,18 @@
+package com.practice.vvr.controller.exception;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+}
