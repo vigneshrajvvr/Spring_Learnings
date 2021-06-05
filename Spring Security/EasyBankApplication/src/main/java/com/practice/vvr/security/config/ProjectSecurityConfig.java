06399b39fe5a96494c88c10e7995832c126ba293
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-06-05 22:45:26 +0530

    In memory authentication

--- Spring Security/EasyBankApplication/src/main/java/com/practice/vvr/security/config/ProjectSecurityConfig.java
+++ Spring Security/EasyBankApplication/src/main/java/com/practice/vvr/security/config/ProjectSecurityConfig.java
@@ -1,8 +1,10 @@
 package com.practice.vvr.security.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -24,18 +26,18 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 
 		/*
 		 * Custom configuration as per our requirement
-		 * 
-		 * 
-		 * http .authorizeRequests() .antMatchers("/myAccount").authenticated()
-		 * .antMatchers("/myBalance").authenticated()
-		 * .antMatchers("/myLoans").authenticated()
-		 * .antMatchers("/myCards").authenticated() .antMatchers("/notices").permitAll()
-		 * .antMatchers("/contact").permitAll(); http.formLogin(); http.httpBasic();
-		 */
+		 */ 
+		 
+		  http .authorizeRequests() .antMatchers("/myAccount").authenticated()
+		  .antMatchers("/myBalance").authenticated()
+		  .antMatchers("/myLoans").authenticated()
+		  .antMatchers("/myCards").authenticated() .antMatchers("/notices").permitAll()
+		  .antMatchers("/contact").permitAll(); http.formLogin(); http.httpBasic();
+		 
 		
 		/*
 		 * formLogin() - Request from browser login form httpBasic() - Request from
-		 * other API
+		 * other APIs
 		 */
 
 		/*
@@ -49,12 +51,27 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 		
 		/*
 		 * Configuration to permit all the requests
+		 *
+		
+		 * http
+		 * 	 .authorizeRequests().anyRequest().permitAll();
+		 * http.formLogin();
+		 * http.httpBasic();
 		 */
-		  http
-		  	 .authorizeRequests().anyRequest().permitAll();
-		  http.formLogin();
-		  http.httpBasic();
 		 
 	}
-
+	
+	/*
+	 * This method should be overridden to provide custom authentication
+	 * It will not accept user which are defined in the application.properties file
+	 * 
+	 */
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("admin").password("12345").authorities("admin").and()
+			.withUser("user").password("12345").authorities("read").and()
+			.passwordEncoder(NoOpPasswordEncoder.getInstance());
+		
+	}
 }
