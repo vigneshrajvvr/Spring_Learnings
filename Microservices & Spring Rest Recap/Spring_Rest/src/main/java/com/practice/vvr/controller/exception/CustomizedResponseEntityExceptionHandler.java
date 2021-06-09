d4474ff93bc4361d5b3532ad098ef3e9a002eb7a
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-06-09 23:47:24 +0530

    Customized exception handler to return customized exception response

---
+++ Microservices & Spring Rest Recap/Spring_Rest/src/main/java/com/practice/vvr/controller/exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.practice.vvr.controller.exception;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@RestController
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler{
+	
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<Object> handleAllException(Exception ex, WebRequest request) {
+		ExceptionResponse response = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+		
+		return new ResponseEntity(response, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@ExceptionHandler(UserNotFoundException.class)
+	public final ResponseEntity<Object> handleUserNotException(Exception ex, WebRequest request) {
+		ExceptionResponse response = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+		
+		return new ResponseEntity(response, HttpStatus.NOT_FOUND);
+	}
+}
