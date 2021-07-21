71a8481956b4aa57a6c2dc853ce740f480da03fb
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-07-21 21:26:06 +0530

    Create post method for the users

--- Microservices & Spring Rest Recap/Spring_Rest/src/main/java/com/practice/vvr/controller/UserJpaController.java
+++ Microservices & Spring Rest Recap/Spring_Rest/src/main/java/com/practice/vvr/controller/UserJpaController.java
@@ -7,7 +7,6 @@ import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
-import javax.persistence.OneToMany;
 import javax.validation.Valid;
 
 import org.jboss.logging.Logger;
@@ -26,81 +25,102 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.practice.vvr.controller.exception.UserNotFoundException;
 import com.practice.vvr.model.Post;
 import com.practice.vvr.model.User;
+import com.practice.vvr.repository.PostRepository;
 import com.practice.vvr.repository.UserRepository;
 
 @RestController
 public class UserJpaController {
-	
 
 	private final Logger USER_LOGGER = Logger.getLogger(UserJpaController.class);
-	
+
 	@Autowired
 	private UserRepository userRepository;
 	
-	//GET /users
-	//retrieveAllUsers
+	@Autowired
+	private PostRepository postRepository;
+
+	// GET /users
+	// retrieveAllUsers
 	@GetMapping("/jpa/users")
 	public List<User> retrieveAllUsers() {
 		return userRepository.findAll();
 	}
-	
-	//GET /users/{id}
-	//retrieveUser(int id)
+
+	// GET /users/{id}
+	// retrieveUser(int id)
 	@GetMapping("/jpa/users/{id}")
 	public EntityModel<User> retrieveUser(@PathVariable("id") int id) {
 		Optional<User> user = userRepository.findById(id);
-		if(!user.isPresent()) {
-			throw new UserNotFoundException("Id-"+id);
+		if (!user.isPresent()) {
+			throw new UserNotFoundException("Id-" + id);
 		}
-		
-		// To add additional data related to the model		
+
+		// To add additional data related to the model
 		EntityModel<User> model = EntityModel.of(user.get());
-		
+
 		WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
-		
+
 		model.add(linkTo.withRel("all-users"));
-		
+
 		return model;
 	}
-	
-	//input - details of user
-	//output - Created response & return the created uri
+
+	// input - details of user
+	// output - Created response & return the created uri
 	@PostMapping("/jpa/users")
 	public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
-		
+
 		User savedUser = userRepository.save(user);
-				
-		URI location = ServletUriComponentsBuilder
-									.fromCurrentRequest()
-									.path("/{id}")
-									.buildAndExpand(savedUser.getId())
-									.toUri();
-		
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId())
+				.toUri();
+
 		USER_LOGGER.info(location.getPath());
-		
+
 		return ResponseEntity.created(location).build();
 	}
-	
+
 	/*
-	 * When deleting a user, we can return 200 Ok status (return type as void)
-	 * or no content status -> return a response entity with no content status
+	 * When deleting a user, we can return 200 Ok status (return type as void) or no
+	 * content status -> return a response entity with no content status
 	 */
 	@DeleteMapping("/jpa/users/{id}")
 	public void deleteUser(@PathVariable("id") int userId) {
 		userRepository.deleteById(userId);
 	}
-	
-	//GET /users
-	//retrieveAllPosts
+
+	// GET /users
+	// retrieveAllPosts
 	@GetMapping("/jpa/users/{id}/posts")
 	public List<Post> retrieveAllPosts(@PathVariable("id") int id) {
 		Optional<User> user = userRepository.findById(id);
-		
-		if(!user.isPresent()) {
-			throw new UserNotFoundException("Id-"+id);
+
+		if (!user.isPresent()) {
+			throw new UserNotFoundException("Id-" + id);
 		}
-		
+
 		return user.get().getPosts();
 	}
-	
+
+	@PostMapping("/jpa/users/{id}/posts")
+	public ResponseEntity<Object> createPost(@PathVariable("id") int id, @Valid @RequestBody Post post) {
+
+		Optional<User> user = userRepository.findById(id);
+
+		if (!user.isPresent()) {
+			throw new UserNotFoundException("Id-" + id);
+		}
+		
+		post.setUser(user.get());
+		
+		postRepository.save(post);
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(post.getId())
+				.toUri();
+
+		USER_LOGGER.info(location.getPath());
+
+		return ResponseEntity.created(location).build();
+	}
+
 }
--- Microservices & Spring Rest Recap/Spring_Rest/src/main/java/com/practice/vvr/model/Post.java
+++ Microservices & Spring Rest Recap/Spring_Rest/src/main/java/com/practice/vvr/model/Post.java
@@ -34,6 +34,14 @@ public class Post {
 		this.description = description;
 	}
 
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
 	@Override
 	public String toString() {
 		return "Post [id=" + id + ", description=" + description + ", user=" + user.getId() + "]";
