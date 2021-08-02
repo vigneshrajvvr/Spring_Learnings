f896f58e6b7b0aa0e577ae1780ee2a1ec2549982
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-08-02 22:53:10 +0530

    Configuration using properties file

--- Microservices_Docker_Kubernetes_Spring_ Rest_Recap/limits-service/src/main/java/com/practice/vvr/microservices/controller/LimitsController.java
+++ Microservices_Docker_Kubernetes_Spring_ Rest_Recap/limits-service/src/main/java/com/practice/vvr/microservices/controller/LimitsController.java
@@ -1,16 +1,22 @@
 package com.practice.vvr.microservices.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.practice.vvr.microservices.configuration.Configuration;
 import com.practice.vvr.microservices.controller.bean.Limits;
 
 @RestController
 public class LimitsController {
+	
+	@Autowired
+	private Configuration configuration;
 
 	@GetMapping("/limits") 
 	public Limits retrieveLimits() {
-		return new Limits(1,1000); 
+//		return new Limits(1,1000); 
+		return new Limits(configuration.getMinimum(), configuration.getMaximum());
 	}
 	
 }
