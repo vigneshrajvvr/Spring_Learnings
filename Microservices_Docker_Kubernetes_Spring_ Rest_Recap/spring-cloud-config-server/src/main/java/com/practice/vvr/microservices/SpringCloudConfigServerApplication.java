72d3d1e0d57488017a3d75b7603cdfe2f38d5aa5
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-08-04 23:42:02 +0530

    Updated comments

--- Microservices_Docker_Kubernetes_Spring_ Rest_Recap/spring-cloud-config-server/src/main/java/com/practice/vvr/microservices/SpringCloudConfigServerApplication.java
+++ Microservices_Docker_Kubernetes_Spring_ Rest_Recap/spring-cloud-config-server/src/main/java/com/practice/vvr/microservices/SpringCloudConfigServerApplication.java
@@ -2,7 +2,9 @@ package com.practice.vvr.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*
+ * Configuration server for centralized configuration management
+ */
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
 
