fb7b366fcf6fdd9295b23177c3c852447da6c9d8
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-08-10 23:46:55 +0530

    Spring cloud configuration using local git repository

--- Microservices_Docker_Kubernetes_Spring_Rest_Recap/spring-cloud-config-server/src/main/java/com/practice/vvr/microservices/SpringCloudConfigServerApplication.java
+++ Microservices_Docker_Kubernetes_Spring_Rest_Recap/spring-cloud-config-server/src/main/java/com/practice/vvr/microservices/SpringCloudConfigServerApplication.java
@@ -2,9 +2,11 @@ package com.practice.vvr.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 /*
  * Configuration server for centralized configuration management
  */
+@EnableConfigServer	
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
 
