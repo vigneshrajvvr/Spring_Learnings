62e26daff32ac3c3af2f0a5558e1e42bd57d0192
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-08-23 22:26:51 +0530

    Dynamic port property

--- Microservices_Docker_Kubernetes_Spring_Rest_Recap/currency-exchange-service/src/main/java/com/practice/vvr/microservices/controller/CurrencyExchangeController.java
+++ Microservices_Docker_Kubernetes_Spring_Rest_Recap/currency-exchange-service/src/main/java/com/practice/vvr/microservices/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.practice.vvr.microservices.controller;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +13,19 @@ import com.practice.vvr.microservices.entity.CurrencyExchange;
 @RestController
 public class CurrencyExchangeController {
 	
+	@Autowired
+	private Environment environment;
+	
 	@GetMapping("currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retrieveExchangeValue(@PathVariable("from") String from,
 												  @PathVariable("to") String to) {
-		return new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(65.00));
+		
+		CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(65.00));
+		String port = environment.getProperty("local.server.port");
+		
+		currencyExchange.setEnvironment(port);
+		
+		return currencyExchange;
 	}
 
 }
