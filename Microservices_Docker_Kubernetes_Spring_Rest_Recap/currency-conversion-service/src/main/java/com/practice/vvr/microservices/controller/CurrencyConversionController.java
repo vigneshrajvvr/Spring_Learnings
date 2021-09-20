bfcc4e3667929d48f80f12c7f5c96b2b3933da4a
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2021-09-20 19:06:35 +0530

    Spring Cloud Feign for API calls

--- Microservices_Docker_Kubernetes_Spring_Rest_Recap/currency-conversion-service/src/main/java/com/practice/vvr/microservices/controller/CurrencyConversionController.java
+++ Microservices_Docker_Kubernetes_Spring_Rest_Recap/currency-conversion-service/src/main/java/com/practice/vvr/microservices/controller/CurrencyConversionController.java
@@ -3,6 +3,7 @@ package com.practice.vvr.microservices.controller;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,9 @@ import com.practice.vvr.microservices.entity.CurrencyConversion;
 
 @RestController
 public class CurrencyConversionController {
+	
+	@Autowired
+	private CurrencyExchangeProxy proxy;
 
 	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to,
@@ -30,6 +34,17 @@ public class CurrencyConversionController {
 		return new CurrencyConversion(currencyConversion.getId(), from, to, currencyConversion.getConversionMultiple(),
 				quantity, quantity.multiply(currencyConversion.getConversionMultiple()), currencyConversion.getEnvironment());
 
+	}
+	
+	@GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion calculateCurrencyConversionFeign(@PathVariable String from, @PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+		
+		CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+
+		return new CurrencyConversion(currencyConversion.getId(), from, to, currencyConversion.getConversionMultiple(),
+				quantity, quantity.multiply(currencyConversion.getConversionMultiple()), currencyConversion.getEnvironment());
+		
 	}
 
 }
---
+++ Microservices_Docker_Kubernetes_Spring_Rest_Recap/currency-conversion-service/src/main/java/com/practice/vvr/microservices/controller/CurrencyExchangeProxy.java
@@ -0,0 +1,16 @@
+package com.practice.vvr.microservices.controller;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.practice.vvr.microservices.entity.CurrencyConversion;
+
+@FeignClient(name="currency-exchange", url="localhost:8000")
+public interface CurrencyExchangeProxy {
+	
+	@GetMapping("currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversion retrieveExchangeValue(@PathVariable("from") String from,
+												  @PathVariable("to") String to);
+
+}
