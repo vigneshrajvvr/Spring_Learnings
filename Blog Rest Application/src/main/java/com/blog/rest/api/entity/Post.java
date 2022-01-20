b2628c18d6efbe367568b3b9003de36525111af9
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2022-01-20 23:11:09 +0530

    Post resource class and removal of test controller

---
+++ Blog Rest Application/src/main/java/com/blog/rest/api/entity/Post.java
@@ -0,0 +1,33 @@
+package com.blog.rest.api.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(
+        name = "posts", uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})}
+)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY
+    )
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+}
