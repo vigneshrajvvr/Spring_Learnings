e04c03777d2d67e6abed9d10029fe348f5e34b0b
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2022-02-08 22:35:03 +0530

    Added a comment in Post Repository

--- Blog Rest Application/src/main/java/com/blog/rest/api/repository/PostRepository.java
+++ Blog Rest Application/src/main/java/com/blog/rest/api/repository/PostRepository.java
@@ -3,5 +3,8 @@ package com.blog.rest.api.repository;
 import com.blog.rest.api.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/*
+Post table repository interface
+ */
 public interface PostRepository extends JpaRepository<Post, Long> {
 }
