8a84cbc5b54eb41d9eca7e78b608ce63cf686d80
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2022-03-22 13:23:41 +0530

    Refactored PostDto to view comments

--- Blog Rest Application/src/main/java/com/blog/rest/api/entity/Post.java
+++ Blog Rest Application/src/main/java/com/blog/rest/api/entity/Post.java
@@ -1,8 +1,6 @@
 package com.blog.rest.api.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.HashSet;
@@ -12,7 +10,8 @@ import java.util.Set;
 @Table(
         name = "posts", uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})}
 )
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Post {
--- Blog Rest Application/src/main/java/com/blog/rest/api/payload/PostDto.java
+++ Blog Rest Application/src/main/java/com/blog/rest/api/payload/PostDto.java
@@ -1,6 +1,9 @@
 package com.blog.rest.api.payload;
 
+import com.blog.rest.api.entity.Comment;
 import lombok.Data;
+
+import java.util.Set;
 
 @Data
 public class PostDto {
@@ -8,4 +11,5 @@ public class PostDto {
     private String title;
     private String description;
     private String content;
+    private Set<CommentDto> comments;
 }
