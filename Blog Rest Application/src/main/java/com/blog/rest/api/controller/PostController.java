b191647c561caee359306d128466d8db66c81a20
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2022-03-29 13:08:04 +0530

    Validation support for Post entity

--- Blog Rest Application/src/main/java/com/blog/rest/api/controller/PostController.java
+++ Blog Rest Application/src/main/java/com/blog/rest/api/controller/PostController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/api/posts")
 public class PostController {
@@ -21,7 +23,7 @@ public class PostController {
 
     // create blog post
     @PostMapping
-    public ResponseEntity<PostDto> createPosts(@RequestBody PostDto postDto) {
+    public ResponseEntity<PostDto> createPosts(@Valid @RequestBody PostDto postDto) {
         return new ResponseEntity<PostDto>(postService.createPost(postDto), HttpStatus.CREATED);
     }
 
@@ -44,7 +46,7 @@ public class PostController {
 
     // update post by id
     @PutMapping("/{id}")
-    public ResponseEntity<PostDto> updatePost(@RequestBody PostDto postDto, @PathVariable("id") Long id) {
+    public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto, @PathVariable("id") Long id) {
         PostDto postResponse = postService.updatePost(postDto, id);
 
         return ResponseEntity.ok(postResponse);
--- Blog Rest Application/src/main/java/com/blog/rest/api/payload/PostDto.java
+++ Blog Rest Application/src/main/java/com/blog/rest/api/payload/PostDto.java
@@ -3,13 +3,32 @@ package com.blog.rest.api.payload;
 import com.blog.rest.api.entity.Comment;
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.Set;
 
 @Data
 public class PostDto {
+
     private Long id;
+    /*
+      title should not be null or empty
+      title should have at least 2 characters
+   */
+    @NotEmpty
+    @Size(min = 2, message = "Post title should have at least 2 characters")
     private String title;
+    /*
+        post description should not be null or empty
+        post description should have at least 10 characters
+     */
+    @NotEmpty
+    @Size(min = 10, message = "Post description should have at least 10 characters")
     private String description;
+    /*
+        post content should not be null or empty
+     */
+    @NotEmpty
     private String content;
     private Set<CommentDto> comments;
 }
