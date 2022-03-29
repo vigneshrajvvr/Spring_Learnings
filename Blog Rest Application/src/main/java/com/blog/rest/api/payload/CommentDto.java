a31c4e8f97e9c4485a4216953d99fecc1cfaa218
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2022-03-29 13:41:46 +0530

    Validation for Comment entity fields

--- Blog Rest Application/src/main/java/com/blog/rest/api/payload/CommentDto.java
+++ Blog Rest Application/src/main/java/com/blog/rest/api/payload/CommentDto.java
@@ -3,14 +3,32 @@ package com.blog.rest.api.payload;
 import com.blog.rest.api.entity.Comment;
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.Set;
 
 @Data
 public class CommentDto {
 
     private Long id;
+    /*
+        name should not be null or empty
+     */
+    @NotEmpty(message = "Name should not be null or empty")
     private String name;
-    private String body;
+    /*
+        email should not be null or empty
+        email field validation
+     */
+    @NotEmpty(message = "Email should not be null or empty")
+    @Email
     private String email;
-
+    /*
+        comment body should not be null or empty
+        comment must be minimum of 10 characters
+     */
+    @NotEmpty
+    @Size(min = 10, message = "Comment body must be minimum 10 characters")
+    private String body;
 }
