efe31e4a8b314e4a35def6e74d01dee67a5b4ec4
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2022-04-05 12:52:10 +0530

    Users and Roles in database

---
+++ Blog Rest Application/src/main/java/com/blog/rest/api/entity/Role.java
@@ -0,0 +1,21 @@
+package com.blog.rest.api.entity;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private long id;
+    @Column(length = 60)
+    private String name;
+
+}
