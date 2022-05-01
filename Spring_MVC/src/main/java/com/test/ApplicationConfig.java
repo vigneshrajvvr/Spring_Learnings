a9cf7fb985eb4cf2454049eb40e347c5441aa47e
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2022-05-01 12:01:02 +0530

    Spring MVC project test

--- Spring_MVC/src/main/java/com/test/ApplicationConfig.java
+++ Spring_MVC/src/main/java/com/test/ApplicationConfig.java
@@ -9,19 +9,19 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
-@ComponentScan(basePackages = "com.text")
+@ComponentScan(basePackages = "com.test")
 public class ApplicationConfig extends WebMvcConfigurationSupport {
 
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("css/**", "images/**")
-                .addResourceLocations("classpath:/css/", "classpath:/jsp");
+                .addResourceLocations("classpath:/css/", "classpath:/images/");
     }
 
     @Bean
     public InternalResourceViewResolver jspViewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix("/WEB-INF/");
+        viewResolver.setPrefix("/WEB-INF/jsp/");
         viewResolver.setSuffix(".jsp");
         viewResolver.setViewClass(JstlView.class);
         return viewResolver;
--- Spring_MVC/src/main/java/com/test/ApplicationInitializer.java
+++ Spring_MVC/src/main/java/com/test/ApplicationInitializer.java
@@ -3,6 +3,7 @@ package com.test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
@@ -13,9 +14,9 @@ public class ApplicationInitializer implements WebApplicationInitializer {
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
 
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
         context.register(ApplicationConfig.class);
-        ServletRegistration.Dynamic servletRegistration = servletContext.addServlet("mvc", new DispatcherServlet((WebApplicationContext) context));
+        ServletRegistration.Dynamic servletRegistration = servletContext.addServlet("mvc", new DispatcherServlet(context));
         servletRegistration.setLoadOnStartup(1);
         servletRegistration.addMapping("/");
 
