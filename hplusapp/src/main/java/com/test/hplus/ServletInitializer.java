c24e3277dd3539f759406bdd3f0d31b86242100b
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2022-05-01 12:49:46 +0530

    Basics recap

---
+++ hplusapp/src/main/java/com/test/hplus/ServletInitializer.java
@@ -0,0 +1,12 @@
+package com.test.hplus;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(HplusappApplication.class);
+    }
+}
