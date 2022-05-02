0cb788d8f8eb5edc8b01c7aac15934ab27dc7a67
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2022-05-03 00:15:07 +0530

    Deferred result

--- hplusapp/src/main/java/com/test/hplus/controllers/SearchController.java
+++ hplusapp/src/main/java/com/test/hplus/controllers/SearchController.java
@@ -3,10 +3,12 @@ package com.test.hplus.controllers;
 import com.test.hplus.beans.Product;
 import com.test.hplus.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.async.DeferredResult;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -20,14 +22,36 @@ public class SearchController {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private AsyncTaskExecutor executor;
+
+//    @GetMapping("/search")
+//    public Callable<String> search(@RequestParam("search") String search, Model model, HttpServletRequest request) {
+//        System.out.println("in search controller");
+//        System.out.println("search criteria " + search);
+//        System.out.println("Async supported in application: " + request.isAsyncSupported());
+//        System.out.println("Thread from the servlet container: " + Thread.currentThread().getName());
+//
+//        return () -> {
+//            Thread.sleep(3000);
+//            System.out.println("Thread from the spring mvc task executor: " + Thread.currentThread().getName());
+//            List<Product> products = new ArrayList<>();
+//            products = productRepository.searchByName(search);
+//            System.out.println("Product List : " + products.toString());
+//            model.addAttribute("products", products);
+//            return "search";
+//        };
+//    }
+
     @GetMapping("/search")
-    public Callable<String> search(@RequestParam("search") String search, Model model, HttpServletRequest request) {
+    public DeferredResult<String> search(@RequestParam("search") String search, Model model, HttpServletRequest request) {
+        DeferredResult<String> deferredResult = new DeferredResult<>();
         System.out.println("in search controller");
         System.out.println("search criteria " + search);
         System.out.println("Async supported in application: " + request.isAsyncSupported());
         System.out.println("Thread from the servlet container: " + Thread.currentThread().getName());
 
-        return () -> {
+        /*return () -> {
             Thread.sleep(3000);
             System.out.println("Thread from the spring mvc task executor: " + Thread.currentThread().getName());
             List<Product> products = new ArrayList<>();
@@ -35,7 +59,23 @@ public class SearchController {
             System.out.println("Product List : " + products.toString());
             model.addAttribute("products", products);
             return "search";
-        };
+        };*/
+
+        executor.execute(() -> {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Thread from the spring mvc task executor: " + Thread.currentThread().getName());
+            List<Product> products = new ArrayList<>();
+            products = productRepository.searchByName(search);
+            System.out.println("Product List : " + products.toString());
+            model.addAttribute("products", products);
+            deferredResult.setResult("search");
+        });
+
+        return deferredResult;
     }
 
 //    @GetMapping
