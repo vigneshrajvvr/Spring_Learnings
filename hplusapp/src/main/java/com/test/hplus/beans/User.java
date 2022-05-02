472911644887e7d56a1a50de29ca3dbf2dc5e58e
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2022-05-02 13:51:45 +0530

    Field validation and binders

--- hplusapp/src/main/java/com/test/hplus/beans/User.java
+++ hplusapp/src/main/java/com/test/hplus/beans/User.java
@@ -4,6 +4,11 @@ import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.util.Date;
 
 @Entity
 @Data
@@ -11,11 +16,15 @@ public class User {
 
     @Id
     private int id;
+    @Size(min = 6, message = "{username.cannot.be.less.than.six.characters}")
     private String username;
+    @Pattern(regexp = "((?=.*[A-Z]).{6,10})", message = "Password must have one upper case, one lower case and should be between 6 and 10 characters")
     private String password;
     private String gender;
+    @NotNull(message = "Activity cannot be left empty")
     private String activity;
+    @NotEmpty(message = "First name cannot be empty")
     private String firstName;
     private String lastName;
-    private String dateOfBirth;
+    private Date dateOfBirth;
 }
